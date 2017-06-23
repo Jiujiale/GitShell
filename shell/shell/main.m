5055bd7540dfa7b1ad245885f3bc93a9b542fdb6
CapricornWolf <1069345726@qq.com>
2017-06-23 12:39:14 +0800

    test

---
+++ shell/shell/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  shell
+//
+//  Created by Jason on 2017/6/23.
+//  Copyright © 2017年 com.sichuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
